5873409c50ef47e5fd60d542c8022f5309f64be0
wkimunya <136619749+wkimunya@users.noreply.github.com>
2023-12-11 19:33:10 +0300

    completed assignment

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,24 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        try:
+            response = requests.get(self.url)
+            response.raise_for_status() 
+            return response.content  
+        except requests.exceptions.RequestException as e:
+            print(f"Error: {e}")
+            return None
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        if response_body:
+            try:
+                json_data = json.loads(response_body)
+                return json_data
+            except json.JSONDecodeError as e:
+                print(f"Error decoding JSON: {e}")
+                return None
+        else:
+            return None
+
+
